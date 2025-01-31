5483b0b2d578a3802b3b2de4c59a493cc2c53ba1
Leonardo Andrade <leonandrade22@gmail.com>
2025-01-31 16:09:13 -0300

    feat: Add Client routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(ClientController::class)->group(function () {
+    Route::get('/clients', 'index');
+    Route::get('/clients/{id}', 'show');
+    Route::post('/clients', 'store');
+    Route::delete('/clients', 'destroy');
+});
